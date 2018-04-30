75c4527a4591e0b22aa564913c2456a4b3f20006
Johanni Brea <johannibrea@gmail.com>
2018-04-30 11:39:26 +0200

    simplify atari, add bprost features (to be tested)

--- environments/atari.jl
+++ environments/atari.jl
@@ -9,54 +9,147 @@ T.callback!
 struct AtariEnv
     ale::Ptr{Void}
     screen::Array{UInt8, 1}
+    getscreen::Function
 end
-function AtariEnv(name)
+import ArcadeLearningEnvironment.getScreen
+function getScreen(p::Ptr, s::Array{Cuchar, 1})
+    sraw = getScreen(p)
+    for i in 1:length(s)
+        s[i] =  sraw[i] .>> 1
+    end
+end
+function AtariEnv(name; 
+                  colorspace = "Grayscale",
+                  frame_skip = 4,
+                  color_averaging = true,
+                  repeat_action_probability = 0.)
     if isfile(name)
         ale = ALE_new()
         loadROM(ale, name)
+        setBool(ale, "color_averaging", color_averaging)
+        setInt(ale, "frame_skip", Int32(frame_skip))
+        setFloat(ale, "repeat_action_probability", 
+                 Float32(repeat_action_probability))
     else
         error("ROM $name could not be found.")
     end
-    screen = Array{UInt8}(210*160*3)
-    AtariEnv(ale, screen)
+    if colorspace == "Grayscale"
+        screen = Array{Cuchar}(210*160)
+        getscreen = getScreenGrayscale
+    elseif colorspace == "RGB"
+        screen = Array{Cuchar}(3*210*160)
+        getscreen = getScreenRGB
+    elseif colorspace == "Raw"
+        screen = Array{Cuchar}(210*160)
+        getscreen = getScreen
+    end
+    AtariEnv(ale, screen, getscreen)
 end
 function interact!(a, env::AtariEnv)
     r = act(env.ale, Int32(a - 1))
-    getScreenRGB(env.ale, env.screen)
+    env.getscreen(env.ale, env.screen)
     env.screen, r, game_over(env.ale)
 end
 function getstate(env::AtariEnv)
-    getScreenRGB(env.ale, env.screen)
+    env.getscreen(env.ale, env.screen)
     env.screen, game_over(env.ale)
 end
 reset!(env::AtariEnv) = reset_game(env.ale)
 
 struct AtariPreprocessor end
 function preprocessstate(::AtariPreprocessor, s)
-    colorimg = colorview(RGB, reshape(normedview(s), 3, 160, 210))
-    Flux.gpu(reshape(Float64.(imresize(Gray.(colorimg), 84, 84)), 84, 84, 1))
+    Flux.gpu(reshape(Float64.(imresize(reshape(s, 160, 210), 84, 84)), 84, 84, 1))
 end
 function preprocessstate(p::AtariPreprocessor, ::Void)
     Flux.gpu(zeros(Float64, 84, 84, 1))
 end
 
-mutable struct RepeatActionPolicy{T}
-    i::Int64
-    k::Int64
-    a::Int64
-    policy::T
-end # TODO: how to reset at end of episode?
-RepeatActionPolicy(; k = 4, a = 1, policy = EpsilonGreedyPolicy(1.)) = 
-    RepeatActionPolicy(0, k, a, policy)
-function selectaction(learner::T.DQN, 
-                      p::RepeatActionPolicy, state)
-    p.i += 1
-    if p.i == p.k
-        p.i = 0
-        p.a = selectaction(learner, p.policy, state)
-    else
-        p.a
-    end
+mutable struct AtariBPROST
+    background::Array{UInt8, 1}
+    lastbasicfeatures::Array{Int64, 1}
+    t::Int64
+    determinebackgrounduntil::Int64
+    backgroundbuffer::Array{Array{UInt8, 1}, 1}
 end
-callback!(c::T.LinearDecreaseEpsilon, learner, p::RepeatActionPolicy, args...) = 
-    callback!(c, learner, p.policy, args...)
+function AtariBPROST(; determinebackgrounduntil = 18000)
+    AtariBPROST(zeros(UInt8, 160 * 210), Int64[], 0,
+                determinebackgrounduntil, Array{UInt8, 1}[])
+end
+function basicfeatures(p, s)
+    ϕ = Int64[]
+    for i in 1:length(s)
+        if s[i] == p.background[i] || s[i] == 0
+            continue
+        else
+            y = div(i, 160)
+            x = mod(i, 160)
+            c = div(x, 10)
+            r = div(y, 15)
+            push!(ϕ, toindbasic(c, r, s[i]))
+        end
+    end
+    unique(ϕ)
+end
+toindbasic(c, r, k) = 1 + c + 16 * r + 16 * 14 * (k - 1)
+function fromindbasic(i)
+    c = (i - 1) % 16
+    rk = div(i - c - 1, 16)
+    r = rk % 14
+    k = div(rk - r, 14) + 1
+    (c, r, k)
+end
+function toindbpros(k1, k2, cdiff, rdiff)
+    if k2 < k1
+        tmp = k1
+        k1 = k2
+        k2 = tmp
+        cdiff = -cdiff
+        rdiff = -rdiff
+    end
+    cdiff + 16 + 31 * (rdiff + 13) + 
+    31 * 27 * ((k1 - 1) * 128 - div((k1 - 1)*(k1 - 2), 2) + k2 - k1)
+end
+function bprosfeatures(ϕ)
+    ϕbpros = Int64[]
+    for i in 1:length(ϕ)
+        @inbounds for j in i+1:length(ϕ)
+            c1, r1, k1 = fromindbasic(ϕ[i])
+            c2, r2, k2 = fromindbasic(ϕ[j])
+            push!(ϕbpros, toindbpros(k1, k2, c1 - c2, r1 - r2))
+        end
+    end
+    ϕbpros
+end
+toindbprot(k1, k2, cdiff, rdiff) = cdiff + 16 + 31 * (rdiff + 13) + 
+   31 * 27 * (k1 - 1) + 31 * 27 * 128 * (k2 - 1)
+function bprotfeatures(ϕ1, ϕ2)
+    ϕbprot = Int64[]
+    for i in 1:length(ϕ1)
+        @inbounds for j in 1:length(ϕ2)
+            c1, r1, k1 = fromindbasic(ϕ1[i])
+            c2, r2, k2 = fromindbasic(ϕ2[j])
+            push!(ϕbprot, toindbprot(k1, k2, c1 - c2, r1 - r2))
+        end
+    end
+    ϕbprot
+end
+
+preprocessstate(p::AtariBPROST, s::Void) = sparsevec([1], [0.], 20652352)
+function preprocessstate(p::AtariBPROST, s)
+    p.t += 1
+    if p.t < p.determinebackgrounduntil
+        push!(p.backgroundbuffer, deepcopy(s))
+    elseif p.t == p.determinebackgrounduntil
+        push!(p.backgroundbuffer, deepcopy(s))
+        p.background = UInt8.(median(hcat(p.backgroundbuffer...), 2)[:])
+        p.lastbasicfeatures = basicfeatures(p, s)
+    else
+        ϕ = basicfeatures(p, s)
+        ϕbpros = bprosfeatures(ϕ)
+        ϕbprot = bprotfeatures(p.lastbasicfeatures, ϕ)
+        p.lastbasicfeatures = ϕ
+        return sparsevec([ϕ; ϕbpros + 28672; ϕbprot + 6938944], 
+                  ones(length(ϕ) + length(ϕbpros) + length(ϕbprot)), 20652352)
+    end
+    sparsevec([1], [0.], 20652352)
+end
--- examples/atari.jl
+++ examples/atari.jl
@@ -2,7 +2,7 @@ include(joinpath(Pkg.dir("TabularReinforcementLearning"),
                  "environments", "atari.jl"))
 
 using TabularReinforcementLearning, Flux
-using CuArrays
+# using CuArrays
 env = AtariEnv("examples/atarirom_files/breakout.bin")
 na = length(getMinimalActionSet(env.ale))
 learner = DQN(Chain(Conv((8, 8), 4 => 32, relu, stride = (4, 4)), 
@@ -10,13 +10,25 @@ learner = DQN(Chain(Conv((8, 8), 4 => 32, relu, stride = (4, 4)),
                          Conv((3, 3), 64 => 64, relu),
                          x -> reshape(x, :, size(x, 4)),
                          Dense(3136, 512, relu), Linear(512, na)),
-              updatetargetevery = 10^4, replaysize = 10^6, nmarkov = 4,
+              updatetargetevery = 10^4, replaysize = 35*10^4, nmarkov = 4,
               startlearningat = 50000, minibatchsize = 14);
-x = RLSetup(Agent(learner, policy = RepeatActionPolicy(), 
+x = RLSetup(Agent(learner, policy = EpsilonGreedyPolicy(1.), 
                   preprocessor = AtariPreprocessor(),
-                  callback = ListofCallbacks([Progress(),
+                  callback = ListofCallbacks([Progress(100),
                              LinearDecreaseEpsilon(5 * 10^4, 10^6, 1, .1)])), 
             env,
             EvaluationPerEpisode(TotalReward()),
-            ConstantNumberSteps(200000));
+            ConstantNumberSteps(60000));
 @time learn!(x)
+
+env = AtariEnv("examples/atarirom_files/breakout.bin", colorspace = "Raw")
+na = length(getMinimalActionSet(env.ale))
+learner2 = Sarsa(na = na, ns = 20652352, λ = .9, γ = .99, α = .005, 
+                 tracekind = ReplacingTraces)
+x2 = RLSetup(Agent(learner2, policy = EpsilonGreedyPolicy(.01),
+             preprocessor = AtariBPROST(),
+             callback = Progress(100)),
+             env,
+             EvaluationPerEpisode(TotalReward()),
+             ConstantNumberSteps(60000));
+@time learn!(x2)
